5a7c1a6c768b6ba722e333f9862583241675509b
20120068 <phanduy0000123@gmail.com>
2024-12-23 20:16:58 +0700

    update pagination

--- backend/controllers/moviesOfCateController.js
+++ backend/controllers/moviesOfCateController.js
@@ -1,10 +1,9 @@
 const getModelForCollection = require('../models/MoviesOfCate');
 
-const MovieUpcoming = getModelForCollection('movies_upcoming');
-const MovieTopRated = getModelForCollection('movies_top_rated');
-const MoviePopular = getModelForCollection('movies_popular');
-const MovieNowPlaying = getModelForCollection('movies_now_playing');
 const MovieSimilar= getModelForCollection('similar');
+
+const Movie = require('../models/Movies');
+
 
 
 exports.getSimilarMovies = async (req, res) => {
@@ -22,36 +21,90 @@ exports.getSimilarMovies = async (req, res) => {
 
 
 exports.getUpcomingMovies = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   try {
-    const movies = await MovieUpcoming.find();
-    res.json(movies);
+    const movies = await Movie.find({ categories: 'upcoming' })
+                              .skip(skip)
+                              .limit(limit);
+    const totalMovies = await Movie.countDocuments({ categories: 'upcoming' });
+    res.json({
+        total: totalMovies,
+        page,
+        limit,
+        totalPages: Math.ceil(totalMovies / limit),
+        data: movies
+    });
   } catch (error) {
     res.status(500).send(error.toString());
   }
 };
+
 
 exports.getTopRatedMovies = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   try {
-    const movies = await MovieTopRated.find();
-    res.json(movies);
+    const movies = await Movie.find({ categories: 'top_rated' }) 
+                              .skip(skip)
+                              .limit(limit);
+    const totalMovies = await Movie.countDocuments({ categories: 'top_rated' });
+    res.json({
+        total: totalMovies,
+        page,
+        limit,
+        totalPages: Math.ceil(totalMovies / limit),
+        data: movies
+    });
   } catch (error) {
     res.status(500).send(error.toString());
   }
 };
 
+
 exports.getPopularMovies = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   try {
-    const movies = await MoviePopular.find();
-    res.json(movies);
+    const movies = await Movie.find({ categories: 'popular' })
+                              .skip(skip)
+                              .limit(limit);
+    const totalMovies = await Movie.countDocuments({ categories: 'popular' });
+    res.json({
+        total: totalMovies,
+        page,
+        limit,
+        totalPages: Math.ceil(totalMovies / limit),
+        data: movies
+    });
   } catch (error) {
     res.status(500).send(error.toString());
   }
 };
 
 exports.getNowPlayingMovies = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   try {
-    const movies = await MovieNowPlaying.find();
-    res.json(movies);
+    const movies = await Movie.find({ categories: 'now_playing' })
+                              .skip(skip)
+                              .limit(limit);
+    const totalMovies = await Movie.countDocuments({ categories: 'now_playing' });
+    res.json({
+        total: totalMovies,
+        page,
+        limit,
+        totalPages: Math.ceil(totalMovies / limit),
+        data: movies
+    });
   } catch (error) {
     res.status(500).send(error.toString());
   }
--- backend/controllers/trendingMoviesController.js
+++ backend/controllers/trendingMoviesController.js
@@ -4,18 +4,45 @@ const MoviesTrendingDays = getModelForCollection('movies_trending_day');
 const MoviesTrendingWeek = getModelForCollection('movies_trending_week');
 
 exports.getTrendingMoviesDay = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   try {
-    const movies = await MoviesTrendingDays.find({});
-    res.json(movies);
+    const movies = await MoviesTrendingDays.find({})
+                                          .skip(skip)
+                                          .limit(limit);
+    const totalMovies = await MoviesTrendingDays.countDocuments({});
+    res.json({
+        total: totalMovies,
+        page,
+        limit,
+        totalPages: Math.ceil(totalMovies / limit),
+        data: movies
+    });
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
+
 exports.getTrendingMoviesWeek = async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   try {
-    const movies = await MoviesTrendingWeek.find({});
-    res.json(movies);
+    const movies = await MoviesTrendingWeek.find({})
+                                           .skip(skip)
+                                           .limit(limit);
+    const totalMovies = await MoviesTrendingWeek.countDocuments({});
+    res.json({
+        total: totalMovies,
+        page,
+        limit,
+        totalPages: Math.ceil(totalMovies / limit),
+        data: movies
+    });
   } catch (error) {
     res.status(500).send(error.message);
   }
