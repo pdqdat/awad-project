ab38855638bf01fa1894a0d2d6b9d4d125f8198e
Dat Phan <phanduongquocdat273@gmail.com>
2024-12-28 15:34:58 +0700

    Refactor calendar component icons to use updated icon components

--- frontend/components/ui/calendar.tsx
+++ frontend/components/ui/calendar.tsx
@@ -60,18 +60,8 @@ function Calendar({
                 ...classNames,
             }}
             components={{
-                IconLeft: ({ className, ...props }) => (
-                    <ChevronLeft
-                        className={cn("h-4 w-4", className)}
-                        {...props}
-                    />
-                ),
-                IconRight: ({ className, ...props }) => (
-                    <ChevronRight
-                        className={cn("h-4 w-4", className)}
-                        {...props}
-                    />
-                ),
+                IconLeft: () => <ChevronLeftIcon className="w-5 h-5" />,
+                IconRight: () => <ChevronRightIcon className="w-5 h-5" />,
             }}
             {...props}
         />
