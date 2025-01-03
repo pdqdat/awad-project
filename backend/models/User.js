5261e03fcd1ad0fe30272f10155ac13cf5510f63
20120068 <phanduy0000123@gmail.com>
2025-01-03 11:05:11 +0700

    llm

---
+++ backend/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        clerkUserId: { type: String, unique: true, required: true },
+        firstName: String,
+        lastName: String,
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema, 'user');
+
+module.exports = User;
