b86cd7e763f8e8b13863acd66bab80e475e91a8b
20120068 <phanduy0000123@gmail.com>
2025-01-04 01:03:36 +0700

    watchlist, favlist, ratinglist

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,10 +5,28 @@ const userSchema = new mongoose.Schema(
         clerkUserId: { type: String, unique: true, required: true },
         firstName: String,
         lastName: String,
+        watchlist: [
+            {
+                movieId: { type: Number, required: true },
+                addedAt: { type: Date, default: Date.now }
+            }
+        ],
+        favoriteList: [
+            {
+                movieId: { type: Number, required: true }, 
+                addedAt: { type: Date, default: Date.now }
+            }
+        ],
+        ratingList: [
+            {
+                movieId: { type: Number, required: true },
+                rating: { type: Number, required: true, min: 0, max: 10 },
+                ratedAt: { type: Date, default: Date.now }
+            }
+        ]
     },
     { timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema, 'user');
-
 module.exports = User;
