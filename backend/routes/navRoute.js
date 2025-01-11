f852a5e326ae93e5cd53b0729e33efa1070e902e
20120068 <phanduy0000123@gmail.com>
2025-01-11 14:03:24 +0700

    require Auth for Nav

--- backend/routes/navRoute.js
+++ backend/routes/navRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { ClerkExpressRequireAuth  } =  require('@clerk/clerk-sdk-node')
 const router = express.Router();
 const navigationController = require('../controllers/navigationController');
 
 // Route to handle navigation queries
-router.post('/navigate', navigationController.getNavigation);
+router.post('/navigate',ClerkExpressRequireAuth(), navigationController.getNavigation);
 
 module.exports = router;
