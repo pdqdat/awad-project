e3ef147e378fe6ae289ec82f6964cab36030a8a0
20120068 <phanduy0000123@gmail.com>
2025-01-11 13:40:02 +0700

    testpage fe

--- frontend/app/(public)/test/page.tsx
+++ frontend/app/(public)/test/page.tsx
@@ -1,4 +1,11 @@
+import { auth } from "@clerk/nextjs/server";
+
 const TestPage = async () => {
+    const { getToken } = await auth();
+
+    const token = await getToken();
+    console.log(token);
+
     return <div className="container min-h-96">Testing stuff..</div>;
 };
 
