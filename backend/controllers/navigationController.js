646dee75cd9caddf9017fc02c1346b3d3652b1f3
20120068 <phanduy0000123@gmail.com>
2025-01-12 17:25:10 +0700

    ai movie page

--- backend/controllers/navigationController.js
+++ backend/controllers/navigationController.js
@@ -17,41 +17,45 @@ exports.getNavigation = async (req, res) => {
     try {
         const response = await axios.post(apiUrl);
         if (response.data.status === 200 && response.data.data.is_success) {
-            if (response.data.data.route === 'CAST_PAGE') {
-                const movieIds = response.data.data.params.movie_ids;
-                // console.log(Movie); // Check the output of the Movie object
-                const movies = await Movie.find({
-                    '_id': { $in: movieIds }
-                }, {
-                    _id: 1,
-                    tmdb_id: 1,
-                    adult: 1,
-                    backdrop_path: 1,
-                    belongs_to_collection: 1,
-                    genres: 1,
-                    homepage: 1,
-                    id: 1,
-                    imdb_id: 1,
-                    overview: 1,
-                    popularity: 1,
-                    poster_path: 1,
-                    release_date: 1,
-                    runtime: 1,
-                    title: 1,
-                    vote_average: 1,
-                    vote_count: 1
-                });
-                res.json({
-                    route: response.data.data.route,
-                    params: movies, // Add movie details to the response
-                    metadata: response.data.data.metadata
-                });
-            } else {
-                res.json({
-                    route: response.data.data.route,
-                    params: response.data.data.params,
-                    metadata: response.data.data.metadata
-                });
+            // Handle response based on the route
+            switch (response.data.data.route) {
+                case 'CAST_PAGE':
+                case 'MOVIE_PAGE':
+                    const movieIds = response.data.data.params.movie_ids;
+                    const movies = await Movie.find({
+                        '_id': { $in: movieIds }
+                    }, {
+                        _id: 1,
+                        tmdb_id: 1,
+                        adult: 1,
+                        backdrop_path: 1,
+                        belongs_to_collection: 1,
+                        genres: 1,
+                        homepage: 1,
+                        id: 1,
+                        imdb_id: 1,
+                        overview: 1,
+                        popularity: 1,
+                        poster_path: 1,
+                        release_date: 1,
+                        runtime: 1,
+                        title: 1,
+                        vote_average: 1,
+                        vote_count: 1
+                    });
+                    res.json({
+                        route: response.data.data.route,
+                        params: movies, // Add movie details to the response
+                        metadata: response.data.data.metadata
+                    });
+                    break;
+                default:
+                    res.json({
+                        route: response.data.data.route,
+                        params: response.data.data.params,
+                        metadata: response.data.data.metadata
+                    });
+                    break;
             }
         } else {
             res.status(response.data.status).json({ message: 'Navigation failed', details: response.data });
